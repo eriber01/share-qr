db6354420bd8b5ca7cbe0fde6760f562d0a6d3b5
eriber <tejeda.e@dhn.com.do>
2024-11-22 13:22:22 -0400

    save point

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return <div className='flex justify-center items-center h-screen my-5 sm:my-0'>
+    <SignUp />
+  </div>
 }
--- src/components/Home/ShareButton.tsx
+++ src/components/Home/ShareButton.tsx
@@ -1,15 +1,17 @@
 'use client'
+import Link from 'next/link'
 import { Button } from '@material-tailwind/react'
 
 export const ShareButton = () => {
   return (
     <div>
-      <Button
+      <Link
+        href={'/dashboard'}
         color="purple"
         className="w-auto inline-flex h-10 items-center justify-center rounded-md bg-[#6d3cbcc5] px-8 text-sm text-white shadow transition-colors hover:bg-[#6E3CBC]"
       >
         Start to share
-      </Button>
+      </Link>
     </div>
   )
 }
