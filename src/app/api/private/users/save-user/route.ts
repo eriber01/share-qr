7be9b267fd05c55d5f547cc337630dd9c70e5a06
Eriber Tejeda <59887012+eriber01@users.noreply.github.com>
2024-11-26 09:07:02 -0400

    save point

--- src/app/api/private/users/save-user/route.ts
+++ src/app/api/private/users/save-user/route.ts
@@ -1,11 +1,26 @@
 import { currentUser } from "@clerk/nextjs/server";
+import prisma from '../../../../../services/prisma';
+import { User } from "@prisma/client";
 
 export async function PUT() {
 
   try {
+    const user = await prisma.user.findMany()
+    console.log({ user });
 
     const userClerk = await currentUser()
     console.log(userClerk);
+
+    if (!userClerk) {
+      return Response.json({ message: 'No found a User' }, { status: 500 })
+    }
+
+    const payload:User = {
+      firstName: userClerk?.firstName!,
+      lastName: userClerk?.lastName!,
+      userId: userClerk.id,
+
+    }
 
     return Response.json({ user: userClerk }, { status: 200 })
 
